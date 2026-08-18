682b79f48128c0ee9d5b31a55fd7172e77a18b66
shashankpastapure-glitch <shashankpastapure@gmail.com>
2026-08-18 11:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (94.62%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int value(char c)
+{
+    if (c == 'I') return 1;
+    if (c == 'V') return 5;
+    if (c == 'X') return 10;
+    if (c == 'L') return 50;
+    if (c == 'C') return 100;
+    if (c == 'D') return 500;
+    if (c == 'M') return 1000;
+
+    return 0;
+} 
+    int romanToInt(string s) {
+        int result =0;
+        for (int i=0; i<s.length();i++){
+            if (i + 1 < s.length() && value(s[i]) < value(s[i + 1]))
+            result -= value(s[i]);
+        else
+            result += value(s[i]);
+
+        }
+
+        return result;
+    }
+};
