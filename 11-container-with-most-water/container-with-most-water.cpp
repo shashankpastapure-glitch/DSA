2ef8abdbe6104c1aeda10155623c7f7ebac15a35
shashankpastapure-glitch <shashankpastapure@gmail.com>
2026-08-23 00:04:36 +0530

    Time: 3 ms (34.54%) | Memory: 62.9 MB (49.37%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int left = 0;
+        int right = height.size() - 1;
+        int result = 0;
+
+        while (left < right) {
+            int h = min(height[left], height[right]);
+            int width = right - left;
+            int area = h * width;
+
+            result = max(result, area);
+        
+            if (height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+
+        return result;
+    }
+};
