06cf2e4419de4a77f41e2f042a87bc07ca7e3135
shashankpastapure-glitch <shashankpastapure@gmail.com>
2026-08-22 23:10:13 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (68.55%) - LeetSync

---
+++ 3918-check-divisibility-by-digit-sum-and-product/check-divisibility-by-digit-sum-and-product.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkDivisibility(int n) {
+        int sum=0;
+        int product=1;
+        int temp=n;
+        while (temp>0){
+            sum += temp%10;
+            product *= temp%10;
+            temp = temp/10;
+        }
+        if (n%(sum+product)==0){
+            return true;
+        }    
+        return false;
+
+    }
+};
