093b74a9573cf01e4155372cef563d30e10d16c6
shashankpastapure-glitch <shashankpastapure@gmail.com>
2026-08-27 18:56:39 +0530

    Time: 0 ms (100.00%) | Memory: 20.7 MB (58.87%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        unordered_map<int, int> freq;
-        for (int x : nums) {
-            freq[x]++;
+        int a=0;
+        for (int i:nums){
+            a^=i;
         }
-        for (pair<int, int> p : freq) { 
-            if (p.second == 1) {
-                return p.first;
-            }
-        }
-        return 0;
+        return a;
     }
 };
