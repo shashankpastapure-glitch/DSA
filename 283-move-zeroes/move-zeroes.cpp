16308a304e789881904773f92cb42c5ccb5f25dc
shashankpastapure-glitch <shashankpastapure@gmail.com>
2026-08-29 21:49:01 +0530

    Time: 3 ms (14.64%) | Memory: 23.9 MB (19.47%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int i = 0;
-        while (i < nums.size()) {
-            if (nums[i] == 0) {
-                int j = i + 1;
-                while (j < nums.size() && nums[j] == 0) {
-                    j++;
-                }
-                if (j < nums.size()) {
-                    swap(nums[i], nums[j]);
-                }
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
             }
-            i++;
         }
     }
 };
