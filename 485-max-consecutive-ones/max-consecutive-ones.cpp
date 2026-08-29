b4322479e241242df66e33261b26914fb00f5f6c
shashankpastapure-glitch <shashankpastapure@gmail.com>
2026-08-29 22:33:30 +0530

    Time: 0 ms (100.00%) | Memory: 50.3 MB (5.12%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int result=0;
+        int count=0;
+        for (int i=0;i<nums.size();i++){
+            if (nums[i]==1){
+                count=count+1;
+                result = max(result, count);
+            }
+            else{
+                count=0;
+            }
+        }
+        return result;
+    }
+};
