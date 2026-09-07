e79aad466f6eb10cd851f94a45550f6f9f8981bc
shashankpastapure-glitch <shashankpastapure@gmail.com>
2026-09-07 22:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (14.27%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -3,11 +3,22 @@ public:
     int mySqrt(int x) { 
         if (x==0) return 0;
         if (x==1) return 1;
-        for (long long int i=1;i<=(x/2)+1;i++){
-            if (i*i>x){
-                return i-1;
+        long long low = 1;
+        long long high = x / 2;
+        int ans = 0;
+
+        while (low <= high) {
+            long long mid = (low + high) / 2;
+
+            if (mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
             }
         }
-        return 0;
+
+        return ans;
     }
 };
