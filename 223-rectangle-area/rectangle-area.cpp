f43f23591aa945a2439c7ed31a48c8a7a85e8ff7
shashankpastapure-glitch <shashankpastapure@gmail.com>
2026-09-14 19:17:01 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (72.81%) - LeetSync

--- 223-rectangle-area/rectangle-area.cpp
+++ 223-rectangle-area/rectangle-area.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
-        int area1= abs((ay2-ay1)*(ax2-ax1));
-        int area2= abs((by2-by1)*(bx2-bx1));
+        int area1= (ay2-ay1)*(ax2-ax1);
+        int area2= (by2-by1)*(bx2-bx1);
         int width = max(0, min(ax2, bx2) - max(ax1, bx1));
         int height = max(0, min(ay2, by2) - max(ay1, by1));
         int intersection = width * height;        
