bafee44395f33d9a6abbb224aacd10e8d834056d
shashankpastapure-glitch <shashankpastapure@gmail.com>
2026-09-15 15:49:10 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (39.50%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    void backtrack(vector<int>& candidates, int target, int start,
+                   vector<int>& current,
+                   vector<vector<int>>& ans) {
+
+        if (target == 0) {
+            ans.push_back(current);
+            return;
+        }
+
+        for (int i = start; i < candidates.size(); i++) {
+
+            if (i > start && candidates[i] == candidates[i - 1])
+                continue;
+
+            if (candidates[i] > target)
+                break;
+
+            current.push_back(candidates[i]);
+
+            backtrack(candidates, target - candidates[i],
+                      i + 1, current, ans);
+
+            current.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum2(vector<int>& candidates,
+                                        int target) {
+
+        sort(candidates.begin(), candidates.end());
+
+        vector<vector<int>> ans;
+        vector<int> current;
+
+        backtrack(candidates, target, 0, current, ans);
+
+        return ans;
+    }
+};
