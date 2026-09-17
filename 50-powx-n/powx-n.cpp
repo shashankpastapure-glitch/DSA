fa6e449c4d05e1d8216ea0864f6f8831748de689
shashankpastapure-glitch <shashankpastapure@gmail.com>
2026-09-17 16:25:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.46%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long power = n;
+        double ans = 1;
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        while (power > 0) {
+            if (power % 2 == 1)
+                ans *= x;
+
+            x *= x;
+            power /= 2;
+        }
+
+        return ans;
+    }
+};
